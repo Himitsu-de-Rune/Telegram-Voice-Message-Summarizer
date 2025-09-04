c42666aada2fd6bc1f5b6bbadfb46d4b371ca905
Himitsu-de-Rune <alex2k13d@gmail.com>
2025-09-04 15:28:43 +0300

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -3,5 +3,5 @@ from transformers import pipeline
 def summarize(text: str) -> str:
     summarizer = pipeline("summarization", model="cointegrated/rut5-base-multitask")
     summary = summarizer(text, max_length=100, min_length=1, do_sample=False)
-    print(summary)
+
     return(summary[0]['summary_text'])
